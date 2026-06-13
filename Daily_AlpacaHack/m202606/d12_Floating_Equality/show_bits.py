ea78292e2f13ab7970e7496bd0281e3a037ed184
baumroll0928-spec <baumroll0928@gmail.com>
2026-06-13 11:10:27 +0900

    Create show_bits.py

---
+++ Daily_AlpacaHack/m202606/d12_Floating_Equality/show_bits.py
@@ -0,0 +1,22 @@
+def bits(x_str:str):
+    dot_pos = x_str.find(".")
+    if dot_pos < 0:
+        i = x_str
+        f = "0"
+    else:
+        i = x_str[:dot_pos]
+        f = x_str[dot_pos + 1:]
+    s = f"{int(i):b}."
+    n = int(f.ljust(24, "0"))
+    m = 24
+    base = pow(10, m)
+    for _ in range(m):
+        n *= 2
+        s += str(n // base)
+        n %= base
+    return s
+
+x = bits("3.14")
+print(x)
+di = x.find(".")
+print("digits of integer:", di)
